2232d3606a6c7f5965a64782fbf975a13f231cdd
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2024-03-23 11:48:16 +0530

    Add files via upload

--- workk.py
+++ workk.py
@@ -23,7 +23,7 @@ if(a1.shape[0]>=200):
     a1['MA_200'] = a1['Close'].rolling(window=200).mean()
 
     a1['Sell_Signal'] = 0
-    a1.loc[(a1['MA_20'] < a1['MA_200']) & (a1['MA_20'].shift(1) > a1['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a1.loc[(a1['MA_20'] < a1['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a1.index, a1['MA_20'], label='20-day MA')
     plt.plot(a1.index, a1['MA_200'], label='200-day MA')
@@ -35,7 +35,7 @@ if(a1.shape[0]>=200):
     plt.show()
     if(a1.shape[0]>=500):
         a1['MA_50'] = a1['Close'].rolling(window=50).mean()
-        a1['MA_500'] = (a1['Close']).rolling(window=200).mean()
+        a1['MA_500'] = (a1['Close']).rolling(window=500).mean()
         a1['Signal'] = 0
 
         a1.loc[a1['MA_50'] >a1['MA_500'], 'Signal'] = 1
@@ -53,7 +53,7 @@ a1['MA_10'] = a1['Close'].rolling(window=10).mean()
 a1['MA_20'] = a1['Close'].rolling(window=20).mean()
 
 a1['Close_Signal'] = 0
-a1.loc[(a1['MA_10'] < a1['MA_20']) & (a1['MA_10'].shift(1) > a1['MA_20'].shift(1)), 'Close_Signal'] = 1
+a1.loc[(a1['MA_10'] < a1['MA_20']), 'Close_Signal'] = 1
 a1.index=a1['Date']
 for index, row in a1.iterrows():
     if row['Close_Signal'] == 1:
@@ -66,7 +66,7 @@ a1['MA_5'] = a1['Close'].rolling(window=5).mean()
 a1['MA_10'] = a1['Close'].rolling(window=10).mean()
 
 a1['Close_Signal'] = 0
-a1.loc[(a1['MA_5'] > a1['MA_10']) & (a1['MA_5'].shift(1) < a1['MA_10'].shift(1)), 'Close_Signal'] = 1
+a1.loc[(a1['MA_5'] > a1['MA_10']) , 'Close_Signal'] = 1
 for index, row in a1.iterrows():
     if row['Close_Signal'] == 1:
      print(f"Closing sell position on {row['Date'].date()} at price {row['Close']}")
@@ -76,7 +76,7 @@ if(a2.shape[0]>=200):
     a2['MA_200'] = a2['Close'].rolling(window=200).mean()
 
     a2['Sell_Signal'] = 0
-    a2.loc[(a2['MA_20'] < a2['MA_200']) & (a2['MA_20'].shift(1) > a2['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a2.loc[(a2['MA_20'] < a2['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a2.index, a2['MA_20'], label='20-day MA')
     plt.plot(a2.index, a2['MA_200'], label='200-day MA')
@@ -88,7 +88,7 @@ if(a2.shape[0]>=200):
     plt.show()
     if(a2.shape[0]>=500):
         a2['MA_50'] = a2['Close'].rolling(window=50).mean()
-        a2['MA_500'] = (a2['Close']).rolling(window=200).mean()
+        a2['MA_500'] = (a2['Close']).rolling(window=500).mean()
         a2['Signal'] = 0
 
         a2.loc[a2['MA_50'] >a2['MA_500'], 'Signal'] = 1
@@ -106,7 +106,7 @@ a2['MA_10'] = a2['Close'].rolling(window=10).mean()
 a2['MA_20'] = a2['Close'].rolling(window=20).mean()
 
 a2['Close_Signal'] = 0
-a2.loc[(a2['MA_10'] < a2['MA_20']) & (a2['MA_10'].shift(1) > a2['MA_20'].shift(1)), 'Close_Signal'] = 1
+a2.loc[(a2['MA_10'] < a2['MA_20']) , 'Close_Signal'] = 1
 a2.index=a2['Date']
 for index, row in a2.iterrows():
     if row['Close_Signal'] == 1:
@@ -120,7 +120,7 @@ a2['MA_5'] = a2['Close'].rolling(window=5).mean()
 a2['MA_10'] = a2['Close'].rolling(window=10).mean()
 
 a2['Close_Signal'] = 0
-a2.loc[(a2['MA_5'] > a2['MA_10']) & (a2['MA_5'].shift(1) < a2['MA_10'].shift(1)), 'Close_Signal'] = 1
+a2.loc[(a2['MA_5'] > a2['MA_10']), 'Close_Signal'] = 1
 
 for index, row in a2.iterrows():
     if row['Close_Signal'] == 1:
@@ -132,7 +132,7 @@ if(a3.shape[0]>=200):
     a3['MA_200'] = a3['Close'].rolling(window=200).mean()
 
     a3['Sell_Signal'] = 0
-    a3.loc[(a3['MA_20'] < a3['MA_200']) & (a3['MA_20'].shift(1) > a3['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a3.loc[(a3['MA_20'] < a3['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a3.index, a3['MA_20'], label='20-day MA')
     plt.plot(a3.index, a3['MA_200'], label='200-day MA')
@@ -144,7 +144,7 @@ if(a3.shape[0]>=200):
     plt.show()
     if(a3.shape[0]>=500):
         a3['MA_50'] = a3['Close'].rolling(window=50).mean()
-        a3['MA_500'] = (a3['Close']).rolling(window=200).mean()
+        a3['MA_500'] = (a3['Close']).rolling(window=500).mean()
         a3['Signal'] = 0
 
         a3.loc[a3['MA_50'] >a3['MA_500'], 'Signal'] = 1
@@ -162,7 +162,7 @@ a3['MA_10'] = a3['Close'].rolling(window=10).mean()
 a3['MA_20'] = a3['Close'].rolling(window=20).mean()
 
 a3['Close_Signal'] = 0
-a3.loc[(a3['MA_10'] < a3['MA_20']) & (a3['MA_10'].shift(1) > a3['MA_20'].shift(1)), 'Close_Signal'] = 1
+a3.loc[(a3['MA_10'] < a3['MA_20']) , 'Close_Signal'] = 1
 a3.index=a3['Date']
 for index, row in a3.iterrows():
         if row['Close_Signal'] == 1:
@@ -175,7 +175,7 @@ print('\n\n\n\nClosing signal for  sell position on AAPL Data\n\n\n\n')
 a3['MA_5'] = a3['Close'].rolling(window=5).mean()
 a3['MA_10'] = a3['Close'].rolling(window=10).mean()
 a3['Close_Signal'] = 0
-a3.loc[(a3['MA_5'] > a3['MA_10']) & (a3['MA_5'].shift(1) < a3['MA_10'].shift(1)), 'Close_Signal'] = 1
+a3.loc[(a3['MA_5'] > a3['MA_10']) , 'Close_Signal'] = 1
 
 for index, row in a3.iterrows():
     if row['Close_Signal'] == 1:
@@ -189,7 +189,7 @@ if(a4.shape[0]>=200):
     a4['MA_200'] = a4['Close'].rolling(window=200).mean()
 
     a4['Sell_Signal'] = 0
-    a4.loc[(a4['MA_20'] < a4['MA_200']) & (a4['MA_20'].shift(1) > a4['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a4.loc[(a4['MA_20'] < a4['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a4.index, a4['MA_20'], label='20-day MA')
     plt.plot(a4.index, a4['MA_200'], label='200-day MA')
@@ -201,7 +201,7 @@ if(a4.shape[0]>=200):
     plt.show()
     if(a4.shape[0]>=500):
         a4['MA_50'] = a4['Close'].rolling(window=50).mean()
-        a4['MA_500'] = (a4['Close']).rolling(window=200).mean()
+        a4['MA_500'] = (a4['Close']).rolling(window=500).mean()
         a4['Signal'] = 0
 
         a4.loc[a4['MA_50'] >a4['MA_500'], 'Signal'] = 1
@@ -219,7 +219,7 @@ a4['MA_10'] = a4['Close'].rolling(window=10).mean()
 a4['MA_20'] = a4['Close'].rolling(window=20).mean()
 
 a4['Close_Signal'] = 0
-a4.loc[(a4['MA_10'] < a4['MA_20']) & (a4['MA_10'].shift(1) > a4['MA_20'].shift(1)), 'Close_Signal'] = 1
+a4.loc[(a4['MA_10'] < a4['MA_20']) , 'Close_Signal'] = 1
 a4.index=a4['Date']
 for index, row in a4.iterrows():
     if row['Close_Signal'] == 1:
@@ -233,7 +233,7 @@ a4['MA_5'] = a4['Close'].rolling(window=5).mean()
 a4['MA_10'] = a4['Close'].rolling(window=10).mean()
 
 a4['Close_Signal'] = 0
-a4.loc[(a4['MA_5'] > a4['MA_10']) & (a4['MA_5'].shift(1) < a4['MA_10'].shift(1)), 'Close_Signal'] = 1
+a4.loc[(a4['MA_5'] > a4['MA_10']), 'Close_Signal'] = 1
 
 for index, row in a4.iterrows():
     if row['Close_Signal'] == 1:
@@ -247,7 +247,7 @@ if(a5.shape[0]>=200):
     a5['MA_200'] = a5['Close'].rolling(window=200).mean()
 
     a5['Sell_Signal'] = 0
-    a5.loc[(a5['MA_20'] < a5['MA_200']) & (a5['MA_20'].shift(1) > a5['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a5.loc[(a5['MA_20'] < a5['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a5.index, a5['MA_20'], label='20-day MA')
     plt.plot(a5.index, a5['MA_200'], label='200-day MA')
@@ -259,7 +259,7 @@ if(a5.shape[0]>=200):
     plt.show()
     if(a5.shape[0]>=500):
         a5['MA_50'] = a5['Close'].rolling(window=50).mean()
-        a5['MA_500'] = (a5['Close']).rolling(window=200).mean()
+        a5['MA_500'] = (a5['Close']).rolling(window=500).mean()
         a5['Signal'] = 0
 
         a5.loc[a5['MA_50'] >a5['MA_500'], 'Signal'] = 1
@@ -277,7 +277,7 @@ a5['MA_10'] = a5['Close'].rolling(window=10).mean()
 a5['MA_20'] = a5['Close'].rolling(window=20).mean()
 
 a5['Close_Signal'] = 0
-a5.loc[(a5['MA_10'] < a5['MA_20']) & (a5['MA_10'].shift(1) > a5['MA_20'].shift(1)), 'Close_Signal'] = 1
+a5.loc[(a5['MA_10'] < a5['MA_20']) , 'Close_Signal'] = 1
 a5.index=a5['Date']
 for index, row in a5.iterrows():
     if row['Close_Signal'] == 1:
@@ -290,7 +290,7 @@ a5['MA_5'] = a5['Close'].rolling(window=5).mean()
 a5['MA_10'] = a5['Close'].rolling(window=10).mean()
 
 a5['Close_Signal'] = 0
-a5.loc[(a5['MA_5'] > a5['MA_10']) & (a5['MA_5'].shift(1) < a5['MA_10'].shift(1)), 'Close_Signal'] = 1
+a5.loc[(a5['MA_5'] > a5['MA_10']) , 'Close_Signal'] = 1
 
 for index, row in a5.iterrows():
     if row['Close_Signal'] == 1:
@@ -299,13 +299,12 @@ for index, row in a5.iterrows():
 
         
 
-   
 if(a6.shape[0]>=200):
     a6['MA_20'] = a6['Close'].rolling(window=20).mean()
     a6['MA_200'] = a6['Close'].rolling(window=200).mean()
 
     a6['Sell_Signal'] = 0
-    a6.loc[(a6['MA_20'] < a6['MA_200']) & (a6['MA_20'].shift(1) > a6['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a6.loc[(a6['MA_20'] < a6['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a6.index, a6['MA_20'], label='20-day MA')
     plt.plot(a6.index, a6['MA_200'], label='200-day MA')
@@ -317,7 +316,7 @@ if(a6.shape[0]>=200):
     plt.show()
     if(a6.shape[0]>=500):
         a6['MA_50'] = a6['Close'].rolling(window=50).mean()
-        a6['MA_500'] = (a6['Close']).rolling(window=200).mean()
+        a6['MA_500'] = (a6['Close']).rolling(window=500).mean()
         a6['Signal'] = 0
 
         a6.loc[a6['MA_50'] >a6['MA_500'], 'Signal'] = 1
@@ -335,7 +334,7 @@ a6['MA_10'] = a6['Close'].rolling(window=10).mean()
 a6['MA_20'] = a6['Close'].rolling(window=20).mean()
 
 a6['Close_Signal'] = 0
-a6.loc[(a6['MA_10'] < a6['MA_20']) & (a6['MA_10'].shift(1) > a6['MA_20'].shift(1)), 'Close_Signal'] = 1
+a6.loc[(a6['MA_10'] < a6['MA_20']) , 'Close_Signal'] = 1
 a6.index=a6['Date']
 for index, row in a6.iterrows():
     if row['Close_Signal'] == 1:
@@ -349,7 +348,7 @@ a6['MA_5'] = a6['Close'].rolling(window=5).mean()
 a6['MA_10'] = a6['Close'].rolling(window=10).mean()
 
 a6['Close_Signal'] = 0
-a6.loc[(a6['MA_5'] > a6['MA_10']) & (a6['MA_5'].shift(1) < a6['MA_10'].shift(1)), 'Close_Signal'] = 1
+a6.loc[(a6['MA_5'] > a6['MA_10']) , 'Close_Signal'] = 1
 
 for index, row in a6.iterrows():
     if row['Close_Signal'] == 1:
@@ -363,7 +362,7 @@ if(a7.shape[0]>=200):
     a7['MA_200'] = a7['Close'].rolling(window=200).mean()
 
     a7['Sell_Signal'] = 0
-    a7.loc[(a7['MA_20'] < a7['MA_200']) & (a7['MA_20'].shift(1) > a7['MA_200'].shift(1)), 'Sell_Signal'] = 1
+    a7.loc[(a7['MA_20'] < a7['MA_200']) , 'Sell_Signal'] = 1
     plt.figure(figsize=(12, 6))
     plt.plot(a7.index, a7['MA_20'], label='20-day MA')
     plt.plot(a7.index, a7['MA_200'], label='200-day MA')
@@ -375,7 +374,7 @@ if(a7.shape[0]>=200):
     plt.show()
     if(a7.shape[0]>=500):
         a7['MA_50'] = a7['Close'].rolling(window=50).mean()
-        a7['MA_500'] = (a7['Close']).rolling(window=200).mean()
+        a7['MA_500'] = (a7['Close']).rolling(window=500).mean()
         a7['Signal'] = 0
 
         a7.loc[a7['MA_50'] >a7['MA_500'], 'Signal'] = 1
@@ -394,7 +393,7 @@ a7['MA_10'] = a7['Close'].rolling(window=10).mean()
 a7['MA_20'] = a7['Close'].rolling(window=20).mean()
 
 a7['Close_Signal'] = 0
-a7.loc[(a7['MA_10'] < a7['MA_20']) & (a7['MA_10'].shift(1) > a7['MA_20'].shift(1)), 'Close_Signal'] = 1
+a7.loc[(a7['MA_10'] < a7['MA_20']) , 'Close_Signal'] = 1
 a7.index=a7['Date']
 for index, row in a7.iterrows():
     if row['Close_Signal'] == 1:
@@ -407,7 +406,7 @@ a7['MA_5'] = a7['Close'].rolling(window=5).mean()
 a7['MA_10'] = a7['Close'].rolling(window=10).mean()
 
 a7['Close_Signal'] = 0
-a7.loc[(a7['MA_5'] > a7['MA_10']) & (a7['MA_5'].shift(1) < a7['MA_10'].shift(1)), 'Close_Signal'] = 1
+a7.loc[(a7['MA_5'] > a7['MA_10']), 'Close_Signal'] = 1
 
 for index, row in a7.iterrows():
     if row['Close_Signal'] == 1:
